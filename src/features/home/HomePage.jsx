bb7af6a706dcde2555fc706163dfc997ab28b737
jaindu <jaindusasandezoysa@gmail.com>
2026-01-12 21:49:40 +0530

    add fast loading

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -197,7 +197,7 @@ export default function HomePage() {
             <div className="mt-4 flex items-center justify-between">
               <div>
                 <div className="text-lg font-bold text-[#105050]">USD {p.price.toLocaleString()}</div>
-                <div className="text-xs text-[#556565]">TWO person </div>
+                <div className="text-xs text-[#556565]">Two Person </div>
               </div>
               <div>
                 <Link to={`/packages/${createSlug(p.name)}`} className="inline-block bg-[#105050] text-white px-4 py-2 rounded-full text-sm font-semibold  transition">
